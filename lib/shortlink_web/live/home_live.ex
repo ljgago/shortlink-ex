f7d820ecf19946d724186ab4103f57fbbb993591
Leonardo Gago <ljgago@gmail.com>
2024-04-19 19:36:35 -0300

    Refactor home_live
    - Usage only form variables.

--- lib/shortlink_web/live/home_live.ex
+++ lib/shortlink_web/live/home_live.ex
@@ -5,14 +5,13 @@ defmodule ShortlinkWeb.HomeLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    form = to_form(%{"long_url" => ""})
+    form =
+      to_form(%{
+        "long_url" => nil,
+        "short_url" => nil
+      })
 
-    {:ok,
-     assign(socket,
-       form: form,
-       long_url: nil,
-       short_url: nil
-     )}
+    {:ok, assign(socket, form: form)}
   end
 
   @impl true
@@ -21,58 +20,64 @@ defmodule ShortlinkWeb.HomeLive do
     <.flash_group flash={@flash} />
     <div class="mx-6 grid place-items-center">
       <div class="flex flex-col gap-2 md:w-[720px] w-full">
-        <h1 class="py-10 text-center text-6xl">
+        <h1 class="py-20 text-center text-6xl">
           Shortlink
         </h1>
         <div class="flex flex-col gap-6">
           <.form for={@form} phx-submit="generate">
             <div class="flex flex-col gap-6">
-              <div class="flex flex-col gap-2">
-                <span><.icon name="hero-link-micro" /> Your Long URL</span>
+              <div>
+                <div class="mb-2">
+                  <.icon name="hero-link-micro" /> Your Long URL
+                </div>
                 <.input
                   id="long-url-id"
                   type="text"
                   autocomplete="off"
-                  readonly={@short_url != nil}
+                  readonly={@form[:short_url].value != nil}
                   field={@form[:long_url]}
-                  value={@long_url}
                 />
               </div>
-              <%= if @short_url == nil do %>
-                <.button class="h-[42px]">
+
+              <%= if @form[:short_url].value != nil do %>
+                <div>
+                  <div class="mb-2">
+                    <.icon name="hero-sparkles-micro" /> Your Short URL
+                  </div>
+                  <.input
+                    id="short-url-id"
+                    type="text"
+                    autocomplete="off"
+                    name="result"
+                    readonly={@form[:short_url].value != nil}
+                    field={@form[:short_url]}
+                  />
+                </div>
+              <% end %>
+
+              <%= if @form[:short_url].value == nil do %>
+                <.button type="submit" class="h-[42px]">
                   Shorten URL
                 </.button>
+              <% else %>
+                <div class="grid grid grid-cols-[1fr,1fr] justify-stretch gap-2">
+                  <.button type="reset" class="h-[42px]" phx-click="new-url">
+                    New URL
+                  </.button>
+                  <.button
+                    id="copy"
+                    class="h-[42px]"
+                    data-to="#short-url-id"
+                    phx-hook="Copy"
+                    phx-click="copy-clipboard"
+                    value={@form[:short_url].value}
+                  >
+                    Copy to Clipboard
+                  </.button>
+                </div>
               <% end %>
             </div>
           </.form>
-          <%= if @short_url != nil do %>
-            <div class="flex flex-col gap-2">
-              <span><.icon name="hero-sparkles-micro" /> Your Short URL</span>
-              <.input
-                id="short-url-id"
-                type="text"
-                autocomplete="off"
-                readonly={@short_url != nil}
-                name="result"
-                value={@short_url}
-              />
-            </div>
-            <div class="grid grid grid-cols-[1fr,1fr] justify-stretch gap-2">
-              <.button class="h-[42px]" phx-click="new-url">
-                New URL
-              </.button>
-              <.button
-                id="copy"
-                class="h-[42px]"
-                data-to="#short-url-id"
-                phx-hook="Copy"
-                phx-click="copy-clipboard"
-                value={@short_url}
-              >
-                Copy to Clipboard
-              </.button>
-            </div>
-          <% end %>
         </div>
       </div>
     </div>
@@ -80,7 +85,32 @@ defmodule ShortlinkWeb.HomeLive do
   end
 
   @impl true
-  def handle_event("generate", %{"long_url" => long_url} = params, socket) do
+  def handle_event("generate", params, socket) do
+    {:noreply, generate_link(socket, params)}
+  end
+
+  def handle_event("new-url", _params, socket) do
+    form =
+      to_form(%{
+        "long_url" => nil,
+        "short_url" => nil
+      })
+
+    {:noreply, assign(socket, form: form)}
+  end
+
+  def handle_event("copy-clipboard", %{"value" => short_url}, socket) do
+    Process.send_after(self(), :clear_flash, 5000)
+
+    {:noreply, put_flash(socket, :info, "Short URL Copied: #{short_url}")}
+  end
+
+  @impl true
+  def handle_info(:clear_flash, socket) do
+    {:noreply, clear_flash(socket)}
+  end
+
+  defp generate_link(socket, %{"long_url" => long_url} = params) do
     %{
       long_url: long_url,
       token: generate_token(),
@@ -93,40 +123,17 @@ defmodule ShortlinkWeb.HomeLive do
           URI.append_path(socket.host_uri, "/#{link.token}")
           |> URI.to_string()
 
-        {:noreply,
-         assign(socket,
-           form: to_form(%{"long_url" => ""}, errors: []),
-           long_url: link.long_url,
-           short_url: short_url
-         )}
+        form =
+          to_form(%{
+            "long_url" => long_url,
+            "short_url" => short_url
+          })
+
+        assign(socket, :form, form)
 
       {:error, %{errors: errors}} ->
-        {:noreply,
-         assign(socket,
-           form: to_form(params, errors: errors),
-           long_url: long_url,
-           short_url: nil
-         )}
+        assign(socket, form: to_form(params, errors: errors))
     end
-  end
-
-  def handle_event("new-url", _params, socket) do
-    {:noreply,
-     assign(socket,
-       form: to_form(%{"long_url" => ""}),
-       long_url: nil,
-       short_url: nil
-     )}
-  end
-
-  def handle_event("copy-clipboard", %{"value" => short_url}, socket) do
-    Process.send_after(self(), :clear_flash, 5000)
-    {:noreply, put_flash(socket, :info, "Short URL Copied: #{short_url}")}
-  end
-
-  @impl true
-  def handle_info(:clear_flash, socket) do
-    {:noreply, clear_flash(socket)}
   end
 
   defp generate_token() do
