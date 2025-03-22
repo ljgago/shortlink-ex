82d89cd5c6c1b60bf177290ef65cd21e9a557f70
Leonardo Gago <ljgago@gmail.com>
2025-03-22 12:58:01 -0300

    feat: add list of links from user

--- lib/shortlink_web/live/links_live/index.ex
+++ lib/shortlink_web/live/links_live/index.ex
@@ -1,9 +1,26 @@
 defmodule ShortlinkWeb.LinksLive.Index do
   use ShortlinkWeb, :live_view
 
+  alias Shortlink.Links
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    links =
+      Links.list_links_by_email(socket.assigns.current_user)
+      |> Enum.map(fn link ->
+        short_url =
+          URI.append_path(socket.host_uri, "/#{link.code}")
+          |> URI.to_string()
+
+        %{
+          long_url: link.long_url,
+          short_url: short_url,
+          visit_count: link.visit_count,
+          expire: Calendar.strftime(link.expire, "%Y-%m-%d %H:%M:%S")
+        }
+      end)
+
+    {:ok, assign(socket, links: links)}
   end
 
   @impl true
